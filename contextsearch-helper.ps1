3c1c4def189955bbbfe05ed14f1569e8acca4056
Codexophile <jaliya.chirantha@gmail.com>
2026-08-23 01:55:36 +0530

    json formatting fixes

--- contextsearch-helper.ps1
+++ contextsearch-helper.ps1
@@ -3,6 +3,8 @@ param(
   [Parameter(Mandatory = $true)]
   [string]$InputFile
 )
+  
+. C:\mega\IDEs\powershell\#lib\functions.ps1
 
 Write-Host "Processing file: $InputFile"
 if (!(Test-Path $InputFile)) {
@@ -10,16 +12,22 @@ if (!(Test-Path $InputFile)) {
   exit 1
 }
 
+$RawData = Get-Content $InputFile -Raw
+$JsonData = $RawData | ConvertFrom-Json -AsHashtable
+$JsonDataText = $JsonData | ConvertTo-Json -Depth 100
+$JsonDataText | Set-Content $InputFile -Encoding utf8
+
 $parentPath = Split-Path -Path $InputFile
 
 Push-Location $parentPath
 git add $InputFile
-$diff = git diff --cached --quiet;
+$diff = git diff --cached --quiet
 $diff
 $hasChanges = $LASTEXITCODE -ne 0
-Write-Host "Has changes: $hasChanges"
+Write-Host "`nHas changes: $hasChanges`n"
 if ($hasChanges) {
   $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
   git commit -m "Auto backup: $timestamp"
+  Invoke-Git-Sync
 }
 Pop-Location
